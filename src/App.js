14f90eb02a278fb79f3186c70438242a99280f16
archi14 <archita810@gmail.com>
2021-12-13 19:44:57 +0530

    passing test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello from kakul!
+          learn react
         </a>
       </header>
     </div>
